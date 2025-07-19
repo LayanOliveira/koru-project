dd7ab7975d8d0b04850f1f60111f421b2bceacaf
Kaua <kaua010413@gmail.com>
2025-07-18 21:36:06 -0300

    3-B Convertendo a string 123.45 em um número

---
+++ 3B-Kauã-Filipy.js
@@ -0,0 +1,4 @@
+const texto = "123.45"; //Declarando a String
+const numero = parseFloat(texto); //Passando a String para Numero
+console.log(numero); //Exibindo no Console para Numero
+console.log(typeof numero);  //Exibindo no console o tipo primitivo do valor armazenado na variável
