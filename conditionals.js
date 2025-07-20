4c85a1bbb32a240b241659dcc9e34e6f4c9d0eb5
Geiza Cristine <geizacristineti@gmail.com>
2025-07-20 12:16:51 -0300

    adicionando exercicio 4B- verificação par ou ímpar

---
+++ conditionals.js
@@ -0,0 +1,18 @@
+// Exercício com estruturas condicionais
+// Exercício 4 - B
+
+
+ // B- Verificar se um número é par ou ímpar
+ 
+const number = 7;
+
+// Operador utilizado: (%)- para verificar o resto da divisão por 2
+// Se o resto for 0, o número é par
+if (number % 2 === 0) {
+  console.log("O número é par.");
+} else {
+  // Caso contrário, o número é ímpar
+  console.log("O número é ímpar.");
+}
+
+
